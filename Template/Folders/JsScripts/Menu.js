cc7cca4de604e4ccedee4584a44292a866fc98eb
Allan Fernandes Barreto <allansieghart1@gmail.com>
2018-05-14 19:27:08 -0300

    Initial commit

---
+++ Template/Folders/JsScripts/Menu.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+                  
+    $(".Hamburguer, .ButtonFechar, .Painel").click(function(){
+        $(".Menu, .Painel").animate({width:"toggle"}, 500);
+    });        
+                  
+                  
+                  
+});
